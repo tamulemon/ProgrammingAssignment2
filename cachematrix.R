04d621486317f28480b4626dc943f02ae7fb4966
tamulemon <tamulemon@yahoo.com>
2014-06-17 20:53:38 -0700

    rprog-004_cacheSolve_commit_MChen
    rprog-004_cacheSolve_commit_MChen

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+## makeCacheMatrix returns a list of 4 functions as below
+#setmatrix: save the input matrix to cache
+#getmatrix: retrieve matrix from cache if it is cached
+#setinverse: save the inversion of the matrix to cache
+#getinverse: retrieve the inversion of the amtrix from cache 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv<-matrix(data=NA)
+  setmatrix<-function(y=matrix()){
+    x<<-y
+  }
+  getmatrix<-function() x
+  setinverse<-function(inverse)
+    inv<<-inverse
+  getinverse<-function() inv
+  list(setmatrix=setmatrix, getmatrix=getmatrix, setinverse=setinverse, getinverse=getinverse)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve takes the input of a list
+#when run, it first tries to retrieve the inverse from cache using "getinver()" fuction
+#if the first element retrieved is NA,indicating no inversion has been saved, 
+#it will calculate the inversion by calling "solve" function and save the result to cache using "setinverse()" fuction
+# next time the same matrix is passed to cacheSolve(), it will retrieve the saved inversion from cache
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv<-x$getinverse()
+  if(!is.na(inv[1,1])){
+    message("getting cached inversion")
+    return (inv)
+  } else{
+    inv<-solve(x$getmatrix())
+    x$setinverse(inv)
+    inv
+  }
 }
+
+m<-makeCacheMatrix(matrix(c(1,3,5,7,9,11,12,14,15),3,3))
+cacheSolve(m)
+
+cacheSolve(m)
